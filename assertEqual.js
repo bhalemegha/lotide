df4fe5a0ba797a7b606150d48ff5533f9fbcc516
bhalemegha <meghabhale6@gmail.com>
2021-10-25 23:12:48 +0000

    assert Equal Function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢Assertion Passed: " + actual + "  === " + expected);
+  } else {
+    console.log("🔴Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(22, 1);

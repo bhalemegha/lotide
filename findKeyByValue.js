3d7e8f805f37180d43c49f48a3296d7f535cde7b
bhalemegha <meghabhale6@gmail.com>
2021-10-28 03:43:11 +0000

    find key by value in of an object

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed:  + ${actual} +   ===  + ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed:  + ${actual} +  !==  + ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objToIterate, val){
+  for(let o in objToIterate){
+    if(objToIterate[o] === val){
+      return o;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Ex"), undefined);

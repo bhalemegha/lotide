15a771ecd9af33ef90bf811aa2018fc2b4e0252d
bhalemegha <meghabhale6@gmail.com>
2021-11-01 21:32:24 +0000

    Test Tail of array functionality using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,17 @@
-const assertArraysEqual = require("../assertArraysEqual.js");
-const tail = require("../tail.js");
-assertArraysEqual(tail([5,6,7]) , [6,7]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
-assertArraysEqual(tail([8]), []);
-assertArraysEqual(tail([]), undefined);
+// const assertArraysEqual = require("../assertArraysEqual.js");
+// const tail = require("../tail.js");
+// assertArraysEqual(tail([5,6,7]) , [6,7]);
+// assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
+// assertArraysEqual(tail([8]), []);
+// assertArraysEqual(tail([]), undefined);
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});

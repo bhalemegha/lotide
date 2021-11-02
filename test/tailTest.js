3e19b58de329c00879a6cedf361acc67d0801604
bhalemegha <meghabhale6@gmail.com>
2021-11-02 01:06:03 +0000

    remove comments in tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,3 @@
-// const assertArraysEqual = require("../assertArraysEqual.js");
-// const tail = require("../tail.js");
-// assertArraysEqual(tail([5,6,7]) , [6,7]);
-// assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
-// assertArraysEqual(tail([8]), []);
-// assertArraysEqual(tail([]), undefined);
 
 const assert = require('chai').assert;
 const tail   = require('../tail');
